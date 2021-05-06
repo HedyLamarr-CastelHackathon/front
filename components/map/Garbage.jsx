99d39ef5dfbfc0c2067390c0a31c2b137aa4dcd2
Benjamin Cloquet <benjamin.cloquet@gmail.com>
2021-05-06 14:16:30 +0200

    feat: add garbage marker icon

--- components/map/Garbage.jsx
+++ components/map/Garbage.jsx
@@ -1,21 +1,22 @@
+/* eslint-disable global-require */
 import { Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 
-const Garbage = ({ localisation }) => {
+const Garbage = ({ garbage: { localisation }, zoom }) => {
+  const maxZoom = 18;
+  const scale = zoom / maxZoom;
+  const baseIconSize = 32;
+  const scaledIconSize = baseIconSize * scale;
   const icon = L.icon({
     iconUrl: require('./trash-alt-solid.svg'),
-    iconSize: [64, 64],
-    iconAnchor: [32, 64],
+    iconSize: [scaledIconSize, scaledIconSize],
+    iconAnchor: [scaledIconSize / 2, scaledIconSize],
     popupAnchor: null,
     shadowUrl: null,
     shadowSize: null,
     shadowAnchor: null,
   });
-  return (
-    <Marker position={localisation} icon={icon}>
-      <Popup>hello!</Popup>
-    </Marker>
-  );
+  return <Marker position={localisation} icon={icon} />;
 };
 
 export default Garbage;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+const withImages = require('next-images');
+
+const config = {
   images: {
     domains: ['github.com'],
   },
 
   target: 'serverless',
 };
+
+module.exports = withImages(config);
