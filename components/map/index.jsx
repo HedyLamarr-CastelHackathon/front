e3194f7e7952180348eba858a00b91a8d563469e
Benjamin Cloquet <benjamin.cloquet@gmail.com>
2021-05-06 17:09:29 +0200

    feat: add mobile sized overlay, add loader

--- components/map/index.jsx
+++ components/map/index.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { useMemo } from 'react';
 import dynamic from 'next/dynamic';
+import Loader from 'components/map/MapLoader';
 
 const DynamicMap = (props) => {
   const options = {
-    loading: () => <p>Loading</p>,
+    loading: Loader,
     ssr: false,
   };
   const Map = useMemo(() => dynamic(() => import('./Map.jsx'), options));
